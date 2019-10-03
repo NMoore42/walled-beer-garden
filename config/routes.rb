fd4e3eea0af6927b21b5c8a15e9306257ef9ef96
NMoore42 <nicholas.moore5@icloud.com>
2019-10-03 16:35:15 -0500

    changed register route to add alias

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'login', to: 'users#login'
-  get 'register', to: 'users#new'
-  get 'users/:id', to: 'users#show'
-  get 'users/:id/beers', to: 'beers#index'
+  resources :users, only: [:create, :update, :delete, :show]
+  get '/login', to: 'users#login'
+  get '/register', to: 'users#new', as: 'register'
+  get '/users/:id', to: 'users#show'
+  get '/users/:id/beers', to: 'beers#index'
 end
